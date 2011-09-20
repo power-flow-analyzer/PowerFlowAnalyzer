d878b9f49c7c6e738ab361f7241e5682d91f3012
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-09-20 09:25:01 +0000

    voltage angle added

--- matlab/data/add_flags_bus.m
+++ matlab/data/add_flags_bus.m
@@ -16,12 +16,31 @@ end
 if busData(VM) < busData(VMIN) || busData(VM) > busData(VMAX)
     voltage_failure = true;
 end
-jflag = net.ee.pfanalyzer.model.NetworkFlag('Voltage magnitude (p.u.)');
+jflag = net.ee.pfanalyzer.model.NetworkFlag('Voltage magnitude');
 jflag.addParameter('VM');
 jflag.setWarning(voltage_percentage > 90);
 jflag.setFailure(voltage_failure);
 jflag.setPercentage(voltage_percentage);
 jbus.addFlag(jflag);
 
+
+%% voltage angle
+voltage_angle_min = -30;
+voltage_angle_max = 30;
+voltage_angle_failure = false;
+if busData(VA) >= 0
+    voltage_angle_percentage = busData(VA) / voltage_angle_max * 100;
+else
+    voltage_angle_percentage = busData(VA) / voltage_angle_min * 100;
+end
+if busData(VA) < voltage_angle_min || busData(VA) > voltage_angle_max
+    voltage_angle_failure = true;
+end
+jflag = net.ee.pfanalyzer.model.NetworkFlag('Voltage angle');
+jflag.addParameter('VA');
+jflag.setWarning(voltage_angle_percentage > 90);
+jflag.setFailure(voltage_angle_failure);
+jflag.setPercentage(voltage_angle_percentage);
+jbus.addFlag(jflag);
 end
 
