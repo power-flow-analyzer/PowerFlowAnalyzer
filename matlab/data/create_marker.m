bae8cfed0a8c392fbe6a22bf9b0e8a3a55e83681
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-10-31 19:10:17 +0000

    markers added

---
+++ matlab/data/create_marker.m
@@ -0,0 +1,9 @@
+function [ jelement ] = create_marker( jnetwork )
+%CREATE_bus Summary of this function goes here
+%   Detailed explanation goes here
+
+%% create bus object
+jelement = net.ee.pfanalyzer.model.MarkerElement(jnetwork);
+
+end
+
