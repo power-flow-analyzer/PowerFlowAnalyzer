d72b860e67c3ddb36a3d87ab07c1162785f30e9b
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-08 10:49:18 +0000

    parameter added for setting case ID

--- matlab/data/create_network.m
+++ matlab/data/create_network.m
@@ -1,9 +1,13 @@
-function [ network ] = create_network( )
+function [ network ] = create_network( jnetwork )
 %CREATEnetwork Creates a new container for power flow data used by the
 %power flow viewer
 
 checkjava();
-network = net.ee.pfanalyzer.model.Network;
+if exist('jnetwork', 'var') == 0
+    network = net.ee.pfanalyzer.model.Network;
+else
+    network = net.ee.pfanalyzer.model.Network(jnetwork.getCaseID());
+end
 
 end
 
