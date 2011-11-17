b2f1eef7f79a1738824a5be784bfff31e0779cfa
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-17 18:41:01 +0000

    constants added

--- src/net/ee/pfanalyzer/ui/util/SwingUtils.java
+++ src/net/ee/pfanalyzer/ui/util/SwingUtils.java
@@ -1,8 +1,13 @@
 package net.ee.pfanalyzer.ui.util;
 
+import java.text.DecimalFormatSymbols;
+
 
 public class SwingUtils {
 
+	public final static String DEFAULT_DECIMAL_SEPARATOR = new DecimalFormatSymbols().getDecimalSeparator()+ "";
+	public final static char DEFAULT_DECIMAL_SEPARATOR_CHAR = DEFAULT_DECIMAL_SEPARATOR.charAt(0);
+	
 //	public static Window getTopLevelFrame(Component c) {
 //		return SwingUtilities.getWindowAncestor(c);
 //	}
