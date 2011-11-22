77ac8613fcc1a274653d9f98a6639159af39fed0
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-22 18:22:36 +0000

    flag for voltage angle removed

--- matlab/data/add_flags_bus.m
+++ matlab/data/add_flags_bus.m
@@ -24,23 +24,23 @@ jflag.setPercentage(voltage_percentage);
 jbus.addFlag(jflag);
 
 
-%% voltage angle
-voltage_angle_min = -30;
-voltage_angle_max = 30;
-voltage_angle_failure = false;
-if busData(VA) >= 0
-    voltage_angle_percentage = busData(VA) / voltage_angle_max * 100;
-else
-    voltage_angle_percentage = busData(VA) / voltage_angle_min * 100;
-end
-if busData(VA) < voltage_angle_min || busData(VA) > voltage_angle_max
-    voltage_angle_failure = true;
-end
-jflag = net.ee.pfanalyzer.model.NetworkFlag('Voltage angle');
-jflag.addParameter('VA');
-jflag.setWarning(voltage_angle_percentage >= 95);
-jflag.setFailure(voltage_angle_failure);
-jflag.setPercentage(voltage_angle_percentage);
-jbus.addFlag(jflag);
+% % % %% voltage angle
+% % % voltage_angle_min = -30;
+% % % voltage_angle_max = 30;
+% % % voltage_angle_failure = false;
+% % % if busData(VA) >= 0
+% % %     voltage_angle_percentage = busData(VA) / voltage_angle_max * 100;
+% % % else
+% % %     voltage_angle_percentage = busData(VA) / voltage_angle_min * 100;
+% % % end
+% % % if busData(VA) < voltage_angle_min || busData(VA) > voltage_angle_max
+% % %     voltage_angle_failure = true;
+% % % end
+% % % jflag = net.ee.pfanalyzer.model.NetworkFlag('Voltage angle');
+% % % jflag.addParameter('VA');
+% % % jflag.setWarning(voltage_angle_percentage >= 95);
+% % % jflag.setFailure(voltage_angle_failure);
+% % % jflag.setPercentage(voltage_angle_percentage);
+% % % jbus.addFlag(jflag);
 end
 
