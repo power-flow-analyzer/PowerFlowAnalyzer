96dc83daa2434ad7340958524da10ee4033d68f5
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-25 18:45:34 +0000

    shape size factor added for dynamic shape sizes; shape size is now in double precision

--- src/net/ee/pfanalyzer/ui/shape/ElementShapeProvider.java
+++ src/net/ee/pfanalyzer/ui/shape/ElementShapeProvider.java
@@ -4,9 +4,11 @@ package net.ee.pfanalyzer.ui.shape;
 
 public class ElementShapeProvider {
 	
-	private int networkSize = 20;
-	private int busSize = 16;
-	private int transformerSize = 16;
+	private double networkSize = 25;
+	private double busSize = 16;
+	private double transformerSize = 20;
+	
+	private double shapeSizeFactor = 1;
 	
 //	private Map<String, IElementShape> shapes = new HashMap<String, IElementShape>();
 	
@@ -16,14 +18,22 @@ public class ElementShapeProvider {
 
 	public IElementShape getShape(String shapeID) {
 		if(NetworkShape.ID.equals(shapeID))
-			return new NetworkShape(networkSize);
+			return new NetworkShape(getShapeSize(networkSize));
 		if(DefaultBusShape.ID.equals(shapeID))
-			return new DefaultBusShape(busSize);
+			return new DefaultBusShape(getShapeSize(busSize));
 		if(DefaultBranchShape.ID.equals(shapeID))
 			return new DefaultBranchShape(-1);
 		if(TransformerShape.ID.equals(shapeID))
-			return new TransformerShape(transformerSize);
+			return new TransformerShape(getShapeSize(transformerSize));
 		return null;
+	}
+	
+	private double getShapeSize(double maxSize) {
+		return Math.max(Math.min(maxSize, shapeSizeFactor * maxSize), 4);
+	}
+	
+	public void setShapeSizeFactor(double factor) {
+		shapeSizeFactor = factor;
 	}
 
 //	public IElementShape getShape(AbstractNetworkElement element, int size, boolean selected) {
