c5589279d8dcd49533d5011c717b2e2ac382b005
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-25 18:43:33 +0000

    shape size is now in double precision

--- src/net/ee/pfanalyzer/ui/shape/DefaultBusShape.java
+++ src/net/ee/pfanalyzer/ui/shape/DefaultBusShape.java
@@ -7,23 +7,23 @@ public class DefaultBusShape implements IElementShape {
 	
 	public static String ID = "shape.bus.default";
 	
-	private int m_size;
+	private double m_size;
 	
-	public DefaultBusShape(int size) {
+	public DefaultBusShape(double size) {
 		m_size = size;
 	}
 	
 	@Override
 	public Shape[] getTranslatedShapes(double x1, double y1, double x2, double y2, boolean highlighted) {
-		int halfSize = m_size / 2;
+		double halfSize = m_size / 2.0;
 		Ellipse2D.Double baseShape = new Ellipse2D.Double(
-				(int) x1 - halfSize, (int) y1 - halfSize, m_size, m_size);
+				x1 - halfSize, y1 - halfSize, m_size, m_size);
 		if(highlighted)
 			return new Shape[] {
 				baseShape,
 				new Ellipse2D.Double(
 						x1 - halfSize - 3.0, y1 - halfSize - 3.0, 
-						m_size + 5.0, m_size + 5.0),
+						m_size + 6.0, m_size + 6.0),
 			};
 		else
 			return new Shape[] { baseShape };
