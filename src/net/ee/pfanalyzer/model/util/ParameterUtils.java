188349d878733f480d5f3a63bbbf01eed20c0635
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-11-28 13:20:43 +0000

    new method parseColor

--- src/net/ee/pfanalyzer/model/util/ParameterUtils.java
+++ src/net/ee/pfanalyzer/model/util/ParameterUtils.java
@@ -1,5 +1,7 @@
 package net.ee.pfanalyzer.model.util;
 
+import java.awt.Color;
+
 import net.ee.pfanalyzer.model.data.NetworkParameter;
 import net.ee.pfanalyzer.model.data.NetworkParameterType;
 
@@ -11,5 +13,14 @@ public class ParameterUtils {
 		if(NetworkParameterType.INTEGER.equals(parameter.getType()) && value.endsWith(".0"))
 			return value.substring(0, value.length() - 2);
 		return value;
+	}
+	
+	public static Color parseColor(String text) {
+		String[] rgbText = text.split(",");
+		int[] rgb = new int[4];
+		for (int i = 0; i < Math.min(rgb.length, rgbText.length); i++) {
+			rgb[i] = Integer.parseInt(rgbText[i]);
+		}
+		return new Color(rgb[0], rgb[1], rgb[2]);
 	}
 }
