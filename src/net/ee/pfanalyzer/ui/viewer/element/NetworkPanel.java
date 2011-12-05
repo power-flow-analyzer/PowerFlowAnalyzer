687135bdcb20f062da2b21bb709e77aca4e9f204
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-12-05 17:23:18 +0000

    generators and loads added

--- src/net/ee/pfanalyzer/ui/viewer/element/NetworkPanel.java
+++ src/net/ee/pfanalyzer/ui/viewer/element/NetworkPanel.java
@@ -3,7 +3,6 @@ package net.ee.pfanalyzer.ui.viewer.element;
 import net.ee.pfanalyzer.model.AbstractNetworkElement;
 import net.ee.pfanalyzer.model.Branch;
 import net.ee.pfanalyzer.model.Bus;
-import net.ee.pfanalyzer.model.Generator;
 import net.ee.pfanalyzer.model.Network;
 import net.ee.pfanalyzer.model.data.NetworkParameter;
 import net.ee.pfanalyzer.model.util.ModelDBUtils;
@@ -44,67 +43,41 @@ public class NetworkPanel extends AbstractElementPanel {
 		
 		// add element groups
 		addBusElements(data.getBusses());
+		addElements(data.getElements("generator"), "Generators");
+		addElements(data.getElements("load"), "Loads");
+//		addElements(data.getElements("branch.transformer"), "Transformers");
 		addBranchElements(data.getBranches(), data.getCombinedBusses());
-//		List<CombinedBus> combinedBusses = ElementGroupingUtils.getCombinedBussesPerArea(data.getBusses());
-//		if(combinedBusses.size() > 1) {
-//			addElementGroup(combinedBusses, "Busses per Area");
-//		} else if( (combinedBusses = ElementGroupingUtils.getCombinedBussesByCoordinates(
-//				data.getBusses())).size() > 1) {
-//			addElementGroup(combinedBusses, "Busses per Location");
-//		} else 
-//			if(data.getCombinedBusCount() > 0) { // show combined elements
-//			addElementGroup("Areas");
-//			for (int i = 0; i < data.getCombinedBusCount(); i++) {
-////				if(data.getCombinedBus(i).getBusNodes().size() == 1)
-////					addElementLink(data.getCombinedBus(i).getFirstBus(), AbstractNetworkElement.DISPLAY_DEFAULT);
-////				else
-//					addElementLink(data.getCombinedBus(i));
-//			}
-//			
-//			addElementGroup("Power Lines");
-//			for (int i = 0; i < data.getCombinedBranchCount(); i++) {
-////				if(data.getCombinedBranch(i).getBranches().size() == 1)
-////					addElementLink(data.getCombinedBranch(i).getFirstBranch(), AbstractNetworkElement.DISPLAY_NAME);
-////				else
-//					addElementLink(data.getCombinedBranch(i));
-//			}
-//		} 
-//		else { // show single elements
-			Group group = new Group("Bus Nodes (no location data available)");
-			for (Bus bus: data.getBusses()) {
-				boolean added = false;
-				for (int i = 0; i < data.getCombinedBusCount(); i++) {
-					if(data.getCombinedBus(i).contains(bus)) {
-						added = true;
-						break;
-					}
+		
+		Group group = new Group("Bus Nodes (no location data available)");
+		for (Bus bus: data.getBusses()) {
+			boolean added = false;
+			for (int i = 0; i < data.getCombinedBusCount(); i++) {
+				if(data.getCombinedBus(i).contains(bus)) {
+					added = true;
+					break;
 				}
-				if(added == false)
-					group.addElementLink(bus, AbstractNetworkElement.DISPLAY_DEFAULT);
 			}
-			if(group.getComponentCount() > 0)
-				addElementGroup(group);
-			
-			group = new Group("Branches (no location data available)");
-			for (Branch branch: data.getBranches()) {
-				boolean added = false;
-				for (int i = 0; i < data.getCombinedBranchCount(); i++) {
-					if(data.getCombinedBranch(i).contains(branch)) {
-						added = true;
-						break;
-					}
+			if(added == false)
+				group.addElementLink(bus, AbstractNetworkElement.DISPLAY_DEFAULT);
+		}
+		if(group.getComponentCount() > 0)
+			addElementGroup(group);
+		
+		group = new Group("Branches (no location data available)");
+		for (Branch branch: data.getBranches()) {
+			boolean added = false;
+			for (int i = 0; i < data.getCombinedBranchCount(); i++) {
+				if(data.getCombinedBranch(i).contains(branch)) {
+					added = true;
+					break;
 				}
-				if(added == false)
-					group.addElementLink(branch, AbstractNetworkElement.DISPLAY_NAME);
 			}
-			if(group.getComponentCount() > 0)
-				addElementGroup(group);
+			if(added == false)
+				group.addElementLink(branch, AbstractNetworkElement.DISPLAY_NAME);
+		}
+		if(group.getComponentCount() > 0)
+			addElementGroup(group);
 			
-			addElementGroup("Generators");
-			for (Generator generator: data.getGenerators()) {
-				addElementLink(generator, AbstractNetworkElement.DISPLAY_DEFAULT);
-			}
-//		}
 		finishLayout();
 	}
 }
