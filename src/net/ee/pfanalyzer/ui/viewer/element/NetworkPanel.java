07ebd2e921744f8d0a02301b449adcf20ef7bfeb
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-12-06 15:01:28 +0000

    elements with no location data removed

--- src/net/ee/pfanalyzer/ui/viewer/element/NetworkPanel.java
+++ src/net/ee/pfanalyzer/ui/viewer/element/NetworkPanel.java
@@ -1,8 +1,5 @@
 package net.ee.pfanalyzer.ui.viewer.element;
 
-import net.ee.pfanalyzer.model.AbstractNetworkElement;
-import net.ee.pfanalyzer.model.Branch;
-import net.ee.pfanalyzer.model.Bus;
 import net.ee.pfanalyzer.model.Network;
 import net.ee.pfanalyzer.model.data.NetworkParameter;
 import net.ee.pfanalyzer.model.util.ModelDBUtils;
@@ -47,37 +44,7 @@ public class NetworkPanel extends AbstractElementPanel {
 		addElements(data.getElements("load"), "Loads");
 //		addElements(data.getElements("branch.transformer"), "Transformers");
 		addBranchElements(data.getBranches(), data.getCombinedBusses());
-		
-		Group group = new Group("Bus Nodes (no location data available)");
-		for (Bus bus: data.getBusses()) {
-			boolean added = false;
-			for (int i = 0; i < data.getCombinedBusCount(); i++) {
-				if(data.getCombinedBus(i).contains(bus)) {
-					added = true;
-					break;
-				}
-			}
-			if(added == false)
-				group.addElementLink(bus, AbstractNetworkElement.DISPLAY_DEFAULT);
-		}
-		if(group.getComponentCount() > 0)
-			addElementGroup(group);
-		
-		group = new Group("Branches (no location data available)");
-		for (Branch branch: data.getBranches()) {
-			boolean added = false;
-			for (int i = 0; i < data.getCombinedBranchCount(); i++) {
-				if(data.getCombinedBranch(i).contains(branch)) {
-					added = true;
-					break;
-				}
-			}
-			if(added == false)
-				group.addElementLink(branch, AbstractNetworkElement.DISPLAY_NAME);
-		}
-		if(group.getComponentCount() > 0)
-			addElementGroup(group);
-			
+					
 		finishLayout();
 	}
 }
