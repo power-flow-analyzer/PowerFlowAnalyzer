0d10464fb3bd16213f8f45744cb052a296af8d66
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-12-09 18:48:37 +0000

    viewer actions added

--- src/net/ee/pfanalyzer/ui/viewer/DataViewerContainer.java
+++ src/net/ee/pfanalyzer/ui/viewer/DataViewerContainer.java
@@ -1,15 +1,16 @@
 package net.ee.pfanalyzer.ui.viewer;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+import javax.swing.AbstractButton;
+import javax.swing.Box;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 import net.ee.pfanalyzer.PowerFlowAnalyzer;
 import net.ee.pfanalyzer.model.PowerFlowCase;
@@ -20,6 +21,7 @@ public class DataViewerContainer extends JPanel {
 	private INetworkDataViewer viewer;
 	private PowerFlowCase powerFlowCase;
 	private NetworkViewer powerFlowviewer;
+	private Box buttonPane;
 
 	public DataViewerContainer(INetworkDataViewer viewer, NetworkViewer powerFlowviewer) {
 		super(new BorderLayout());
@@ -27,21 +29,33 @@ public class DataViewerContainer extends JPanel {
 		this.powerFlowCase = powerFlowviewer.getPowerFlowCase();
 		this.powerFlowviewer = powerFlowviewer;
 		
-		JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		buttonPane = Box.createVerticalBox();
+		buttonPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		final JButton editButton = PowerFlowAnalyzer.createButton("Edit viewer properties", 
-				"wrench.png", "Edit viewer properties", false);
-		editButton.setMargin(new Insets(2, 2, 2, 2));
-		editButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				showPropertiesDialog();
-			}
+		addAction("Edit viewer properties", "wrench.png", "Edit viewer properties", false,
+			new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					showPropertiesDialog();
+				}
 		});
-		buttonPane.add(editButton);
+		// add actions from viewer
+		viewer.addViewerActions(this);
 
 		add(buttonPane, BorderLayout.EAST);
 		add(new JScrollPane(viewer.getComponent()), BorderLayout.CENTER);
+	}
+	
+	public AbstractButton addAction(String tooltipText, String iconName, 
+			String altText, boolean toggleAction, ActionListener listener) {
+		AbstractButton button = toggleAction ? 
+				PowerFlowAnalyzer.createToggleButton(tooltipText, iconName, altText, false) : 
+					PowerFlowAnalyzer.createButton(tooltipText, iconName, altText, false);
+		button.setMargin(new Insets(2, 2, 2, 2));
+		button.addActionListener(listener);
+		buttonPane.add(button);
+		buttonPane.add(Box.createVerticalStrut(10));
+		return button;
 	}
 	
 	private void showPropertiesDialog() {
