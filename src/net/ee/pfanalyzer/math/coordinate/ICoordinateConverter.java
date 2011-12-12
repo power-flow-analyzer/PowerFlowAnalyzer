c25920c492930c6013d1293a77b0f66f723544af
Markus Gronau <43175392+mehg@users.noreply.github.com>
2011-12-12 10:15:57 +0000

    documentation fixed

--- src/net/ee/pfanalyzer/math/coordinate/ICoordinateConverter.java
+++ src/net/ee/pfanalyzer/math/coordinate/ICoordinateConverter.java
@@ -3,30 +3,30 @@ package net.ee.pfanalyzer.math.coordinate;
 public interface ICoordinateConverter {
 
 	/**
-	 * Returns the screen coordinate x for the given latitude.
-	 * @param E the latitude
+	 * Returns the screen coordinate x for the given longitude.
+	 * @param E the longitude
 	 * @return screen coordinate x
 	 */
 	int getX(double E);
 	
 	/**
-	 * Returns the screen coordinate y for the given longitude.
-	 * @param N the longitude
+	 * Returns the screen coordinate y for the given latitude.
+	 * @param N the latitude
 	 * @return screen coordinate y
 	 */
 	int getY(double N);
 	
 	/**
-	 * Returns the latitude for the given screen coordinate x.
-	 * @param x screen coordinate x
+	 * Returns the latitude for the given screen coordinate y.
+	 * @param x screen coordinate y
 	 * @return the latitude
 	 */
-	double getLatitude(int x);
+	double getLatitude(int y);
 	
 	/**
-	 * Returns the longitude for the given screen coordinate y.
-	 * @param y screen coordinate y
+	 * Returns the longitude for the given screen coordinate x.
+	 * @param y screen coordinate x
 	 * @return the longitude
 	 */
-	double getLongitude(int y);
+	double getLongitude(int x);
 }
