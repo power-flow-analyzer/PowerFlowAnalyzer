362b4ede8b46f9033815c4cdf28225c868216171
Markus Gronau <43175392+mehg@users.noreply.github.com>
2012-01-16 08:48:55 +0000

    object action and tooltip added

--- src/net/ee/pfanalyzer/ui/util/HyperLinkLabel.java
+++ src/net/ee/pfanalyzer/ui/util/HyperLinkLabel.java
@@ -7,19 +7,34 @@ import net.ee.pfanalyzer.ui.NetworkElementSelectionManager;
 public class HyperLinkLabel extends HyperLinkAction {
 	
 	private Object networkElement;
+	private IObjectAction action;
 	
 	public HyperLinkLabel(String text, Object element) {
+		this(text, element, (IObjectAction) null);
+	}
+	
+	public HyperLinkLabel(String text, Object element, IObjectAction action) {
 		super(text);
 		this.networkElement = element;
+		setToolTipText("Click to open element");
 	}
 	
 	public HyperLinkLabel(String text, Object element, Color foreground) {
+		this(text, element, foreground, null);
+	}
+	
+	public HyperLinkLabel(String text, Object element, Color foreground, IObjectAction action) {
 		super(text, foreground);
 		this.networkElement = element;
+		this.action = action;
+		setToolTipText("Click to open element");
 	}
 
 	@Override
 	protected void actionPerformed() {
-		NetworkElementSelectionManager.selectionChanged(HyperLinkLabel.this, networkElement);
+		if(action != null)
+			action.actionPerformed(networkElement, null);
+		else // backward compatible
+			NetworkElementSelectionManager.selectionChanged(HyperLinkLabel.this, networkElement);
 	}
 }
