38405fbc9065bdb2b5cd6fe75044dc2888969ed0
Markus Gronau <43175392+mehg@users.noreply.github.com>
2012-03-12 19:10:07 +0000

    default value added

--- matlab/parameters/parameter_int.m
+++ matlab/parameters/parameter_int.m
@@ -1,8 +1,12 @@
 function [ value ] = parameter_int( ...
-    network_element, parameter_name )
+    network_element, parameter_name, default_value )
 %PARAMETER_INT Summary of this function goes here
 %   Detailed explanation goes here
 
-value = network_element.getIntParameter(parameter_name, 0);
+if exist('default_value', 'var') == 0
+    default_value = 0;
+end
+
+value = network_element.getIntParameter(parameter_name, default_value);
 end
 
