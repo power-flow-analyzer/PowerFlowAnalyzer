a1bf2e11694c90aa0751d7f04b3b38ff7d2786f0
Markus Gronau <43175392+mehg@users.noreply.github.com>
2012-03-12 19:09:59 +0000

    default value added

--- matlab/parameters/parameter_double.m
+++ matlab/parameters/parameter_double.m
@@ -1,8 +1,12 @@
 function [ value ] = parameter_double( ...
-    network_element, parameter_name )
+    network_element, parameter_name, default_value )
 %PARAMETER_DOUBLE Summary of this function goes here
 %   Detailed explanation goes here
 
-value = network_element.getDoubleParameter(parameter_name, 0);
+if exist('default_value', 'var') == 0
+    default_value = 0;
+end
+
+value = network_element.getDoubleParameter(parameter_name, default_value);
 end
 
