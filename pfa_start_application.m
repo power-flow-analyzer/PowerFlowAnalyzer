d792ce50fd12d372ea05f37a0834bd8158eb3674
Markus Gronau <43175392+mehg@users.noreply.github.com>
2017-10-24 13:30:28 +0200

    initial version

---
+++ pfa_start_application.m
@@ -0,0 +1,13 @@
+clear java; %#ok<CLSCR> % hide annoying warning
+
+javaaddpath([pwd '/java/pfanalyzer.jar']);
+javaaddpath([pwd '/java/miglayout-3.7-swing.jar']);
+javaaddpath([pwd '/java/fatcow-hosting-icons-2000.zip']);
+
+addpath([pwd '/matlab']);
+addpath([pwd '/matlab/communication']);
+addpath([pwd '/matlab/data']);
+addpath([pwd '/matlab/parameters']);
+addpath([pwd '/matlab/viewer']);
+
+PowerFlowAnalyzer_MAIN
---
+++ pfa_start_application_dev.m
@@ -0,0 +1,20 @@
+% clear java; %#ok<CLSCR> % hide annoying warning
+
+javaaddpath([pwd '/bin']);
+javaaddpath([pwd '/java/miglayout-3.7-swing.jar']);
+javaaddpath([pwd '/java/fatcow-hosting-icons-2000.zip']);
+
+addpath([pwd '/matlab']);
+addpath([pwd '/matlab/communication']);
+addpath([pwd '/matlab/data']);
+addpath([pwd '/matlab/datasources']);
+addpath([pwd '/matlab/datasources/DACF']);
+addpath([pwd '/matlab/parameters']);
+addpath([pwd '/matlab/viewer']);
+
+% % points dynamically to subfolder of current user directory, thus 
+% % overwriting library functions above. Directory must not exist in
+% % user directory after all.
+% addpath('scripts');
+
+PowerFlowAnalyzer_MAIN
