ebb29af3f9a7a23246b2eea13e6fd952ce05dbaf
Markus Gronau <43175392+mehg@users.noreply.github.com>
2018-01-15 16:11:39 +0100

    voltage level codes moved to new function

--- matlab/datasources/DACF/dacf2pfa.m
+++ matlab/datasources/DACF/dacf2pfa.m
@@ -290,30 +290,7 @@ end
 
 function [ base_kv ] = node_name_to_base_kv(node_name)
     voltage_level_code = str2double(node_name(length(node_name)-1));
-    switch voltage_level_code
-        case 0
-            base_kv = 750;
-        case 1
-            base_kv = 380;
-        case 2
-            base_kv = 220;
-        case 3
-            base_kv = 150;
-        case 4
-            base_kv = 120;
-        case 5
-            base_kv = 110;
-        case 6
-            base_kv = 70;
-        case 7
-            base_kv = 27;
-        case 8
-            base_kv = 330;
-        case 9
-            base_kv = 500;
-        otherwise
-            error('Unknown voltage level code: %i', voltage_level_code);
-    end
+    base_kv = dacf_voltage_level_to_base_kv(voltage_level_code);
 end
 
 function [ branch_status ] = line_to_branch_status(line_status)
---
+++ matlab/datasources/DACF/dacf_voltage_level_to_base_kv.m
@@ -0,0 +1,26 @@
+function [ base_kv ] = dacf_voltage_level_to_base_kv(voltage_level_code)
+    switch voltage_level_code
+        case 0
+            base_kv = 750;
+        case 1
+            base_kv = 380;
+        case 2
+            base_kv = 220;
+        case 3
+            base_kv = 150;
+        case 4
+            base_kv = 120;
+        case 5
+            base_kv = 110;
+        case 6
+            base_kv = 70;
+        case 7
+            base_kv = 27;
+        case 8
+            base_kv = 330;
+        case 9
+            base_kv = 500;
+        otherwise
+            error('Unknown voltage level code: %i', voltage_level_code);
+    end
+end
