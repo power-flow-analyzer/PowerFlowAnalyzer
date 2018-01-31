094bf9005a2561cf11c10e96140eeae6c39a852f
Markus Gronau <43175392+mehg@users.noreply.github.com>
2018-01-31 15:55:49 +0100

    parameter ignore_islands added

--- matlab/matpower/mp_handle_isolated_buses.m
+++ matlab/matpower/mp_handle_isolated_buses.m
@@ -1,11 +1,15 @@
-function [ mpc ] = mp_handle_isolated_buses( mpc )
+function [ mpc ] = mp_handle_isolated_buses( mpc, ignore_islands )
 %MP_HANDLE_ISOLATED_BUSES Summary of this function goes here
 %   Detailed explanation goes here
 
+if exist('ignore_islands', 'var') == 0
+   ignore_islands = false; 
+end
+
 % find islands and isolated buses
 connected_buses = find_islands(mpc);
-if length(connected_buses) > 1
-    error('Error: %i islands were detected in network', length(connected_buses));
+if length(connected_buses) > 1 && ignore_islands == false
+    error('Error: %i islands were detected in network', length(connected_buses) - 1);
 end
 connected_buses = connected_buses{1};% make it a double vector
 all_buses = 1:size(mpc.bus(:, 1), 1);
