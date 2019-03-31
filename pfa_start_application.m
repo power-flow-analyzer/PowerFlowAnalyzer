6db439c9135ed43f8ed519ab65f344b07db38b94
Markus Gronau <43175392+mehg@users.noreply.github.com>
2019-03-31 16:47:21 +0200

    updated from pfa_start_application_dev.m

--- pfa_start_application.m
+++ pfa_start_application.m
@@ -1,12 +1,15 @@
-clear java; %#ok<CLSCR> % hide annoying warning
+% clear java; %#ok<CLSCR> % hide annoying warning
 
-javaaddpath([pwd '/java/pfanalyzer.jar']);
+javaaddpath([pwd '/java/PowerFlowAnalyzer_2.2.2.jar']);
 javaaddpath([pwd '/java/miglayout-3.7-swing.jar']);
 javaaddpath([pwd '/java/fatcow-hosting-icons-2000.zip']);
 
 addpath([pwd '/matlab']);
 addpath([pwd '/matlab/communication']);
 addpath([pwd '/matlab/data']);
+addpath([pwd '/matlab/datasources']);
+addpath([pwd '/matlab/datasources/UCTE_DEF']);
+addpath([pwd '/matlab/matpower']);
 addpath([pwd '/matlab/parameters']);
 addpath([pwd '/matlab/viewer']);
 
