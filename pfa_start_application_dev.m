a07cfd772b404c4d13afc5d4a67e2169ded68225
Markus Gronau <43175392+mehg@users.noreply.github.com>
2019-04-08 22:13:30 +0200

    scripts folder added to Matlab search path; classpath adapted to Maven

--- pfa_start_application_dev.m
+++ pfa_start_application_dev.m
@@ -17,7 +17,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % clear java; %#ok<CLSCR> % hide annoying warning
 
-javaaddpath([pwd '/bin']);
+javaaddpath([pwd '/target/classes']);
 javaaddpath([pwd '/java/miglayout-3.7-swing.jar']);
 javaaddpath([pwd '/java/fatcow-hosting-icons-2000.zip']);
 
@@ -30,9 +30,9 @@ addpath([pwd '/matlab/matpower']);
 addpath([pwd '/matlab/parameters']);
 addpath([pwd '/matlab/viewer']);
 
-% % points dynamically to subfolder of current user directory, thus 
-% % overwriting library functions above. Directory must not exist in
-% % user directory after all.
-% addpath('scripts');
+% points dynamically to subfolder of current user directory, thus 
+% overwriting library functions above. Directory must not exist in
+% user directory after all.
+addpath('scripts');
 
 PowerFlowAnalyzer_MAIN
